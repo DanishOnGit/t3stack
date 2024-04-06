dc8742e5077bdb0c5bbd5769419978bc7ba58e84
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2024-04-06 20:44:25 +0000

    Add button,heading,input components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+
+
+export const Navbar=()=>{
+
+    return <nav className="px-10 py-3 pb-5">
+        <div className="flex">
+            <ul className="justify-self-end text-xs flex gap-5">
+                <li>Help</li>
+                <li>Orders & Returns</li>
+                <li>Hi, John</li>
+            </ul>
+        </div>
+        <div>
+            <strong>ECOMMERCE</strong>
+            <div>
+            <ul className="justify-self-end text-xs flex gap-5">
+                <li>Help</li>
+                <li>Orders & Returns</li>
+                <li>Hi, John</li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+}

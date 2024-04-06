967fe623b5cce716342d4abe2023fbba56063e18
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2024-04-06 23:03:39 +0000

    Add signup login pages

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,17 +4,21 @@ import { Heading } from "~/components/Heading";
 import { Input } from "~/components/Input";
 
 
-const Signup=()=>{
+const Login=()=>{
     return <div className="border border-gray-300 px-14 py-10 max-w-xl rounded-3xl max-h-dvh mx-auto ">
         <div className="flex flex-col items-center justify-center">
             <div className="mb-8">
-                <Heading title="Create your account"/>
+                <Heading title="Login"/>
+            </div>
+            <div className="mb-8 text-center">
+            <p className="text-2xl font-medium">
+                Welcome back to ECOMMERCE
+            </p>
+            <p className="text-base">
+             The next gen business marketplace
+            </p>
             </div>
         <div className="flex flex-col gap-8 mb-10 w-full">
-        <Input>
-            <Input.InputLabel>Name</Input.InputLabel>
-            <Input.InputBox/>
-        </Input>
         <Input>
             <Input.InputLabel>Email</Input.InputLabel>
             <Input.InputBox/>
@@ -25,14 +29,14 @@ const Signup=()=>{
         </Input>
         </div>
         <div className="w-full mb-12">
-            <Button text="Create Account"/>
+            <Button text="Login"/>
         </div>
         <div>
             <p className="flex gap-2 text-sm">
-                Have an Account?
+                Don’t have an Account? 
             <span className="font-semibold">
                 <Link href={'/login'}>
-                    LOGIN
+                    SIGN UP
                 </Link>
             </span>
             </p>
@@ -42,4 +46,4 @@ const Signup=()=>{
     </div>
 }
 
-export default Signup
+export default Login

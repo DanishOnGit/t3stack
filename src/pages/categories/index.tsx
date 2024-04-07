5e2556c8bf97d8d4e908bdbd42feaba478b00f60
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2024-04-07 09:58:32 +0000

    Add create category and get categories apis

---
+++ src/pages/categories/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router"
+import { Heading } from "~/components/Heading"
+import { api } from "~/utils/api";
+
+const Category=({name}:{name:string})=>{
+
+    return <div>
+        <label>
+            <input type="checkbox" />
+            {name}
+        </label>
+    </div>
+}
+const Categories=()=>{
+    const data = api.categories.getCategories.useQuery();
+    const {mutate}= api.categories.createCategory.useMutation()
+
+    return <div className="border border-gray-300 px-14 py-10 max-w-xl rounded-3xl max-h-dvh mx-auto ">
+    <div className="flex flex-col items-center justify-center">
+        <div className="mb-8">
+            <Heading title="Please mark your interests!"/>
+        </div>
+        <div className="mb-8 text-center">
+        <p className="text-base">
+            We will keep you notified.
+        </p>
+        </div>
+    </div>
+    <div className="flex flex-col gap-8 mb-10 w-full">
+        <p onClick={()=>mutate({name:"danny"})} className="text-xl font-lg">My saved interests!</p>
+
+    </div>
+</div>
+}
+
+export default Categories
--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,16 +1,26 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { PrismaClient } from "@prisma/client";
+import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient()
 
 
 export const categoryRouter = createTRPCRouter({
-    create:publicProcedure.input(z.object({name:z.string().min(1)})).mutation(async({ctx,input})=>{
-        const category = await prisma.category.create({
+    createCategory:publicProcedure.input(z.object({name:z.string().min(1)})).mutation(async({ctx,input})=>{
+       await prisma.categories.create({
             data: {
-              name: 'Furniture',
+              name:faker.commerce.product(),
             },
           })
-          return categories
+          return "Successfully screated"
+    }),
+    getCategories:publicProcedure.query(async({input})=>{
+      const pageSize = 6;
+      const offset = 1 * pageSize;
+      const data = await prisma.categories.findMany({
+        take:pageSize,
+        skip:offset
+      })
+      return data
     })
 })

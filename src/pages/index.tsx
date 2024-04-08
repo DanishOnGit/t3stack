f4764487f3a026c1dc132a48e3e7a902c49aed89
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2024-04-08 00:39:53 +0000

    Redirect to signup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
+  const router = useRouter()
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
+
+  useEffect(()=>{
+    router.push("/signup")
+  },[])
   return (
     <>
       <Head>
